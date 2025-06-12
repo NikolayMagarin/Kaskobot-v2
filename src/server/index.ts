7f857806ea03b66e8f57282e8c42ffe8f3979bc9
Nikolay Magarin <magarinnikolay@gmail.com>
2025-06-12 09:24:00 +0300

    remove self ping entirely

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,7 @@
 import express from 'express';
 import path from 'path';
-import { selfPing } from './ping';
 
 export const server = express();
-
-server.post('/api/ping', selfPing());
 
 const publicPath = path.join(__dirname, '../..', 'public');
 server.use('/', express.static(publicPath));
