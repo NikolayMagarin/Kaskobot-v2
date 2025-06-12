ae2d50245908cd6707d03c1479566247a7885d9c
Nikolay Magarin <magarinnikolay@gmail.com>
2025-06-12 09:44:09 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { launch } from './bot/bot';
 import { loadStartupConfigs } from './db/configLoader';
 import { server } from './server';
-import { ping } from './server/ping';
 
 async function main() {
   await loadStartupConfigs();
 
   launch();
 
-  server.listen(8080, ping);
+  server.listen(8080);
 }
 
 main().catch(console.error);
